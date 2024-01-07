dc8cd79a9a5520b43a5dd4bbc2734b59c4fd2539
IacobOliver <olialexander08@gmail.com>
2024-01-07 13:10:09 +0200

    project initialization

---
+++ src/main/java/cleancode/webnewsbackedn/WebNewsBackednApplication.java
@@ -0,0 +1,13 @@
+package cleancode.webnewsbackedn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNewsBackednApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNewsBackednApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cleancode/webnewsbackedn/WebNewsBackednApplicationTests.java
@@ -0,0 +1,13 @@
+package cleancode.webnewsbackedn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNewsBackednApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
